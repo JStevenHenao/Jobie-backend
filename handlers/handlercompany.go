9bb16902bedff45e2029b3861ad8c71d5f3e5874
JStevenHenao <steven.030814@gmail.com>
2023-05-03 20:02:41 -0500

    Handler Companys

--- handlers/handlercompany.go
+++ handlers/handlercompany.go
@@ -1,1 +1,20 @@
 package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"tidy/db"
+	"tidy/models"
+)
+
+func CreateCompany(rw http.ResponseWriter, r *http.Request) {
+	company := models.Company{}
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&company); err != nil {
+		sendError(rw, http.StatusUnprocessableEntity)
+	} else {
+		db.Database.Save(&company)
+		sendData(rw, company, http.StatusCreated)
+	}
+}
